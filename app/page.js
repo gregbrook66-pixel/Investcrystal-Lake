393c2e4342eb1b74a85d89a6f43d31f5d5fa7882
Investment site <gregbrook66@gmail.com>
2025-12-25 05:59:01 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Investor Participation Tiers</h1>
+      <p>Select a tier to join the community</p>
+      <a href="/investors">View Investors</a>
+    </main>
+  )
+  }
+  
