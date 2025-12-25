dbef384c94c6ef19bf0de224b45e2f3dcc69ea3f
Investment site <gregbrook66@gmail.com>
2025-12-25 06:52:04 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,20 +1,8 @@
-export default function Home() {
-  return (
-    <main style={{ padding: 20 }}>
-      <h1>Investor Participation Platform</h1>
-      <p>
-        Choose a participation tier and join our growing community of early
-        supporters.
-      </p>
-
-      <ul>
-        <li>
-          <a href="/register">Join as an Investor</a>
-        </li>
-        <li>
-          <a href="/investors">View Community Investors</a>
-        </li>
-      </ul>
-    </main>
-  )
-}
+<main style={{ padding: 20 }}>
+  <h1>Investcrystal Platform</h1>
+  <p>
+    Choose a participation tier and join our growing community of early
+    supporters.
+  </p>
+  ...
+</main>
