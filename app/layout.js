0395ec217273b83a87bcb92853ba0157303d282b
Investment site <gregbrook66@gmail.com>
2025-12-25 06:18:17 +0100

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,12 @@
+export default function RootLayout({ children }) {
+  return (
+    <html>
+      <body style={{ fontFamily: 'sans-serif' }}>
+        {children}
+        <footer style={{ fontSize: 12, marginTop: 40, color: '#555' }}>
+          Projected values and bonuses are estimates only and not guaranteed.
+        </footer>
+      </body>
+    </html>
+  )
+}
